bc10c64eda2d2a5c0d91a4c1222e6ec5c51fbc94
MiguelFazenda2001 <128982100+MiguelFazenda2001@users.noreply.github.com>
2025-05-30 17:35:07 +0100

    DungeonGenerator v1

---
+++ Scripts/Main/main.gd
@@ -0,0 +1,201 @@
+extends Node2D
+
+var Room = preload("res://Scenes/Dungeon/Room.tscn")
+var font = preload("res://Assets/Dungeon/Roboto-VariableFont_wdth,wght.ttf")
+var Player = preload("res://Scenes/Player/player.tscn")
+@onready var stone_map = $Grass.get_node("Stone")
+@onready var grass_map = $Grass
+@onready var map_camera = $MapCamera
+
+var tile_size = 10 # size of tile in the TileMap
+var num_rooms = 100 # numer of rooms to generate
+var min_size = 30   # minimum room size ( in tiles)
+var max_size = 60  # maximum room size (in tiles )
+var hspread = 20   # horizontal spread ( in pixels )
+var cull = 0.75     # chance to cull the room
+
+var path           # AStar pathfinding object
+var start_room = null
+var end_room = null
+var play_mode = null
+var player = null
+
+func _ready():
+	randomize()
+	make_rooms()
+	
+func make_rooms():
+	for i in range(num_rooms):
+		var pos = Vector2(randi_range(-hspread, hspread),0)
+		var r = Room.instantiate()
+		var w = min_size + randi() % (max_size - min_size)
+		var h = min_size + randi() % (max_size - min_size)
+		r.make_room(pos, Vector2(w,h) * tile_size)
+		$Rooms.add_child(r)
+	# wait for movement to stop
+	await(get_tree().create_timer(1.1).timeout)
+	
+	var room_positions = []
+	# cull rooms
+	for room in $Rooms.get_children():
+		if randf() < cull:
+			room.queue_free()
+		else:
+			room.freeze = true
+			room_positions.append(room.position)
+	
+	await(get_tree().create_timer(1.1).timeout)
+	
+	# generate a minimum spanning treee connecting the rooms
+	path = find_mst(room_positions)
+			
+func find_mst(nodes):
+	var path = AStar2D.new()
+	path.add_point(path.get_available_point_id(), nodes.pop_front())
+	
+	#repeat until no more nodes remain
+	while nodes:
+		var min_dist = INF # Minimum distance so far
+		var min_p = null   # Position of that node
+		var p = null       # current position
+		for p1 in path.get_point_ids():
+			var p_temp = path.get_point_position(p1)
+			for p2 in nodes:
+				if p_temp.distance_to(p2) < min_dist:
+					min_dist = p_temp.distance_to(p2)
+					min_p = p2
+					p = p_temp
+		
+		var n = path.get_available_point_id()
+		path.add_point(n, min_p)
+		path.connect_points(path.get_closest_point(p), n)
+		nodes.erase(min_p)
+		
+	return path				
+			
+		
+		
+func _draw():
+	if start_room:
+		draw_string(font, start_room.position, "start", 0, -1, 16, Color(1, 1, 1))
+	if end_room:
+		draw_string(font, end_room.position, "end", 0, -1, 16, Color(1, 1, 1))
+	if play_mode:
+		return		
+	for room in $Rooms.get_children():
+		draw_rect(Rect2(room.position - room.size + room.size / 2, room.size), Color(0,1,0), false)
+	if path:
+		for p in path.get_point_ids():
+			for c in path.get_point_connections(p):
+				var pp = path.get_point_position(p)
+				var cp = path.get_point_position(c)
+				draw_line(pp,cp, Color(1,1,0), 5, true)	
+		
+func _process(delta):
+	queue_redraw()
+		
+func _input(event):
+	if event.is_action_pressed('ui_select'):
+		if play_mode:
+			var map_cam = self.get_node("Camera2D")
+			if map_cam:
+				map_cam.make_current()
+			
+			player.queue_free()
+			play_mode = false
+		for n in $Rooms.get_children():
+			n.queue_free()
+		path = null
+		start_room = null
+		end_room = null	
+		make_rooms()
+	if event.is_action_pressed(('ui_focus_next')):
+		make_map()
+	if event.is_action_pressed('ui_cancel'):
+		for room in $Rooms.get_children():
+			room.get_node("CollisionShape2D").disabled = true
+		player = Player.instantiate()
+		add_child(player)
+		player.position = start_room.position
+		
+		await get_tree().create_timer(0.1).timeout
+		
+		var cam = player.get_node("Camera2D")
+		if cam:
+			cam.make_current()
+
+		play_mode = true	
+
+
+func make_map():
+	stone_map.clear()
+	grass_map.clear()
+	find_start_room()
+	find_end_room()
+	
+	# Fill TileMap with walls, then carve empty rooms
+	var full_rect = Rect2()
+	for room in $Rooms.get_children():
+		var r = Rect2(room.position - room.size / 2 , room.size)
+		full_rect = full_rect.merge(r)
+	var top_left = stone_map.local_to_map(full_rect.position)
+	var bottom_right = stone_map.local_to_map(full_rect.end)
+	for x in range(top_left.x, bottom_right.x):
+		for y in range(top_left.y, bottom_right.y):	
+			stone_map.set_cell(Vector2i(x, y), 0, Vector2i(0, 0))
+	
+	# carve rooms
+	var corridors = []
+	for room in $Rooms.get_children():
+		var s = (room.size / tile_size).floor()  # size in tiles
+		var ul = ((room.position - room.size + room.size / 2))/tile_size  # top-left in tile coords
+		
+		for x in range(s.x-1):
+			for y in range(s.y-1):
+				grass_map.set_cell(Vector2i(ul.x + x, ul.y + y), 0, Vector2i(0, 0))
+ 	
+		var p = path.get_closest_point(room.position)
+		for conn in path.get_point_connections(p):
+			if not conn in corridors:
+				var start = grass_map.local_to_map(path.get_point_position(p))
+				var end = grass_map.local_to_map(path.get_point_position(conn))
+				
+				carve_path(start, end)
+		corridors.append(p)
+						 
+func carve_path(pos1, pos2):
+	# Carve a path between 2 points
+	var x_diff = sign(pos2.x - pos1.x)
+	var y_diff = sign(pos2.y - pos1.y)
+	if x_diff == 0: x_diff = pow(-1.0, randi() % 2)
+	if y_diff == 0: y_diff = pow(-1.0, randi() % 2)
+	
+	# choose either x/y or y/x
+	var x_y = pos1
+	var y_x = pos2
+	if (randi() % 2) > 0:
+		x_y = pos2
+		y_x = pos1
+		
+	for x in range(pos1.x, pos2.x, x_diff):
+		grass_map.set_cell(Vector2i(x, x_y.y), 0,  Vector2i(0, 0))
+		grass_map.set_cell(Vector2i(x, x_y.y + y_diff), 0,  Vector2i(0, 0))
+	for y in range(pos1.y, pos2.y, y_diff):
+		grass_map.set_cell(Vector2i(y_x.x, y), 0, Vector2i(0, 0))
+		grass_map.set_cell(Vector2i(y_x.x + x_diff, y), 0, Vector2i(0, 0))
+		
+	
+func find_start_room():
+	var min_x = INF
+	for room in $Rooms.get_children():
+		if room.position.x < min_x:
+			min_x = room.position.x
+			start_room = room
+
+func find_end_room():
+	var max_x = -INF
+	for room in $Rooms.get_children():
+		if room.position.x > max_x:
+			max_x = room.position.x
+			end_room = room		
+	
